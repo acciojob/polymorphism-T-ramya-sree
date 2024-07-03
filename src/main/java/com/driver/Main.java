7f325232a18f4b7e3552b6f0235ca9cceb391f75
Ramya Shree <kramyasree505@gmail.com>
2024-07-03 22:13:10 +0530

    s1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    public  static class product{
+    public  static class Product{
         public int product(int x, int y) {
             return x * y;
         }
@@ -14,10 +14,10 @@ public class Main {
 
     }
     public static void main(String[]args){
-        product p=new product();
+        Product p=new Product();
         int result=p.product(3,4);
         int result1=p.product(5,6,7);
-        double result2=p.product(8,9);
+        double result2=p.product(8.0,9.0);
         System.out.println(result);
         System.out.println(result1);
         System.out.println(result2);
